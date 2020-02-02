39cefb34bbfbefa4197ba98b6a816f7b857950cc
zhizekai <1310248516@qq.com>
2020-02-02 16:46:40 +0800

    跳转修复

--- src/main/java/com/tjpu/zzk/config/SecurityConfig.java
+++ src/main/java/com/tjpu/zzk/config/SecurityConfig.java
@@ -27,94 +27,121 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Resource
     MyAuthenticationFailureHandler myAuthenticationFailureHandler;
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+////        http.csrf().disable()//跨站防御攻击
+////             .formLogin()
+////                .loginPage("/login.html")
+////                .usernameParameter("uname")
+////                .passwordParameter("pword")
+////                .loginProcessingUrl("/login")
+////                //.defaultSuccessUrl("/index")
+////                //.failureUrl("/login.html")
+////                .successHandler(mySuthenticationSuccessHandler)
+////                .failureHandler(myAuthenticationFailureHandler)
+////             .and()
+////             .authorizeRequests()
+////                .antMatchers("/login.html","/login").permitAll()
+////                .antMatchers("/biz1","/biz2") //需要对外暴露的资源路径
+////                    .hasAnyAuthority("ROLE_user","ROLE_admin")  //user角色和admin角色都可以访问
+////                //.antMatchers("/syslog","/sysuser")
+////                    //.hasAnyRole("admin")  //admin角色可以访问
+////                    //.hasAnyAuthority("ROLE_admin")
+////                .antMatchers("/syslog").hasAuthority("sys:log")
+////                .antMatchers("/sysuser").hasAuthority("sys:user")
+////                .anyRequest().authenticated()
+////             .and().sessionManagement()
+////                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+////                .invalidSessionUrl("/login.html")
+////                .sessionFixation().migrateSession()
+////                .maximumSessions(1)
+////                .maxSessionsPreventsLogin(false)
+////                .expiredSessionStrategy(new MyExpiredSessionStrategy());
+//
+//       http.csrf().disable() //禁用跨站csrf攻击防御，后面的章节会专门讲解
+//            .formLogin()
+//            .loginPage("/login.html")//用户未登录时，访问任何资源都转跳到该路径，即登录页面
+//            .usernameParameter("username")///登录表单form中用户名输入框input的name名，不修改的话默认是username
+//            .passwordParameter("password")//form中密码输入框input的name名，不修改的话默认是password
+//               .loginProcessingUrl("/login")//登录表单form中action的地址，也就是处理认证请求的路径
+//            .defaultSuccessUrl("/index")//登录认证成功后默认转跳的路径
+//
+//
+//        .and()
+//            .authorizeRequests()
+//            .antMatchers("/login.html","/login").permitAll()//不需要通过登录验证就可以被访问的资源路径
+//            .antMatchers("/biz1","/biz2") //需要对外暴露的资源路径
+//                .hasAnyAuthority("ROLE_user","ROLE_admin")  //user角色和admin角色都可以访问
+//            .antMatchers("/syslog","/sysuser")
+//                .hasAnyRole("admin")  //admin角色可以访问
+////            .antMatchers("/syslog").hasAuthority("sys:log")
+////            .antMatchers("/sysuser").hasAuthority("sys:user")
+//            .anyRequest().authenticated();
+//    }
+//
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//
-//        http.csrf().disable()
-//             .formLogin()
-//                .loginPage("/login.html")
-//                .usernameParameter("uname")
-//                .passwordParameter("pword")
-//                .loginProcessingUrl("/login")
-//                //.defaultSuccessUrl("/index")
-//                //.failureUrl("/login.html")
-//                .successHandler(mySuthenticationSuccessHandler)
-//                .failureHandler(myAuthenticationFailureHandler)
-//             .and()
-//             .authorizeRequests()
-//                .antMatchers("/login.html","/login").permitAll()
-//                .antMatchers("/biz1","/biz2") //需要对外暴露的资源路径
-//                    .hasAnyAuthority("ROLE_user","ROLE_admin")  //user角色和admin角色都可以访问
-//                //.antMatchers("/syslog","/sysuser")
-//                    //.hasAnyRole("admin")  //admin角色可以访问
-//                    //.hasAnyAuthority("ROLE_admin")
-//                .antMatchers("/syslog").hasAuthority("sys:log")
-//                .antMatchers("/sysuser").hasAuthority("sys:user")
-//                .anyRequest().authenticated()
-//             .and().sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-//                .invalidSessionUrl("/login.html")
-//                .sessionFixation().migrateSession()
-//                .maximumSessions(1)
-//                .maxSessionsPreventsLogin(false)
-//                .expiredSessionStrategy(new MyExpiredSessionStrategy());
-
-//        http.csrf().disable()//跨站防御攻击
-//                .formLogin().loginPage("/login.html")//默认在login访问
-//                .loginProcessingUrl("/login")
-//                .defaultSuccessUrl("/index").and()
-//
-//                //权限控制
-//                .authorizeRequests()
-//                .antMatchers("/login.html","login").permitAll()//这两个不需要登陆就能被访问
-//                .antMatchers("/biz1","/biz2") //需要对外暴露的资源路径
-//                .hasAnyAuthority("ROLE_user","ROLE_admin")  //user角色和admin角色都可以访问
-//                .antMatchers("/syslog","/sysuser") //这两个需要admin权限
-//                .hasAnyRole("admin")  //admin角色可以访问
-//                .anyRequest().authenticated();
-
-        http.csrf().disable().httpBasic()//开启httpbasic认证
+        http.csrf().disable() //禁用跨站csrf攻击防御，后面的章节会专门讲解
+                .formLogin()
+                .loginPage("/login.html")//用户未登录时，访问任何资源都转跳到该路径，即登录页面
+                .loginProcessingUrl("/login")//登录表单form中action的地址，也就是处理认证请求的路径
+//                .usernameParameter("username")///登录表单form中用户名输入框input的name名，不修改的话默认是username
+//                .passwordParameter("password")//form中密码输入框input的name名，不修改的话默认是password
+                .defaultSuccessUrl("/index")//登录认证成功后默认转跳的路径
                 .and()
                 .authorizeRequests()
-                .anyRequest()
-                .authenticated();//所有请求都需要登录认证才能访问
+                .antMatchers("/login.html","/login").permitAll()//不需要通过登录验证就可以被访问的资源路径
+                .antMatchers("/biz1","/biz2") //需要对外暴露的资源路径
+                .hasAnyAuthority("ROLE_user","ROLE_admin")  //user角色和admin角色都可以访问
+                .antMatchers("/syslog","/sysuser")
+                .hasAnyRole("admin")  //admin角色可以访问
+                //.antMatchers("/syslog").hasAuthority("sys:log")
+                //.antMatchers("/sysuser").hasAuthority("sys:user")
+                .anyRequest().authenticated();
+    }
+    /**
+     * 静态用户名和配置
+     * @param auth 权限
+     * @throws Exception 表达
+     */
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                    .withUser("user")
+                    .password(passwordEncoder().encode("123456"))
+                    .roles("user")
+                .and()
+                    .withUser("admin")
+                    .password(passwordEncoder().encode("123456"))
+//                    .authorities("sys:log","sys:user")
+                    .roles("admin")
+                .and()
+                    .passwordEncoder(passwordEncoder());//配置BCrypt加密
     }
 
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user")
-//                .password(passwordEncoder().encode("123456"))
-//                .roles("user")
-//                .and()
-//                .withUser("admin")
-//                .password(passwordEncoder().encode("123456"))
-//                .authorities("sys:log","sys:user")
-//                .roles("admin")
-//                .and()
-//                .passwordEncoder(passwordEncoder());//配置BCrypt加密
-//    }
 
 
-
-//    /**
-//     * 用户名和密码进行加密
-//     * @return 加密函数
-//     */
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
+    /**
+     * 用户名和密码进行加密
+     * @return 加密函数
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 //
-//    /**
-//     * 静态资源不需要权限
-//     * @param web web
-//     */
-//    @Override
-//    public void configure(WebSecurity web) {
-//        //将项目中静态资源路径开放出来
-//        web.ignoring()
-//           .antMatchers( "/css/**", "/fonts/**", "/img/**", "/js/**");
-//    }
+    /**
+     * 静态资源不需要权限
+     * @param web web
+     */
+    @Override
+    public void configure(WebSecurity web) {
+        //将项目中静态资源路径开放出来
+        web.ignoring()
+           .antMatchers( "/css/**", "/fonts/**", "/img/**", "/js/**");
+    }
 
 }
